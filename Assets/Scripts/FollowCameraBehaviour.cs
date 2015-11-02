4654c16c72f36ff9824c9ed5bbc35f8a93086151
openspyro <openspyroproject@gmail.com>
2015-11-02 15:49:36 -0500

    Started implementing the orbit controls for the camera.
    Currently bugged.

--- Assets/Scripts/FollowCameraBehaviour.cs
+++ Assets/Scripts/FollowCameraBehaviour.cs
@@ -16,8 +16,9 @@ public class FollowCameraBehaviour : MonoBehaviour
 
     //Configuration
     private float followingHeight = 3;
-    private float followingMoveSpeed = 10;      //The speed the camera moves at while in following mode.
-    private float followingDistance = 15;        //The distance the camera maintains from the target in following mode.
+    private float followingMoveSpeed = 10;          //The speed the camera moves at while in following mode.
+    private float followingDistance = 15;           //The distance the camera maintains from the target in following mode.
+    private float followingOrbitSpeed = 360f;       //The speed the camera rotates with the right analog stick.
 
     //Events
 
@@ -45,19 +46,50 @@ public class FollowCameraBehaviour : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
     }
 
+    private void MaintainDistance(float height, float distance)
+    {
+        //Updates the target pos to maintain a constance distance from the player.
+
+        Vector3 diff = targetPos - target.transform.position;
+        diff.y = 0;
+        diff.Normalize();
+        diff *= distance;
+        diff.y = height;
+
+        targetPos = target.transform.position + diff;
+    }
+
+    private void OrbitWithStick()
+    {
+        //Orbit around the player with the right-stick
+
+        Vector3 diff = targetPos - target.transform.position;
+
+        //Find the angle to the player
+        float angle = Mathf.Atan2(diff.z, diff.x) * Mathf.Rad2Deg;
+
+        //Find the distance to the player
+        //float dist = Mathf.Sqrt(diff.x * diff.x + diff.z * diff.z);
+        float dist = followingDistance;
+
+        //Rotate the angle
+        angle += Input.GetAxis("RightStick_x") * followingOrbitSpeed * Time.deltaTime;
+
+        //Update the target position.
+        angle *= Mathf.Deg2Rad;
+        targetPos.x = target.transform.position.x + dist * Mathf.Cos(angle);
+        targetPos.z = target.transform.position.x + dist * Mathf.Sin(angle);
+    }
 
     //State methods
 
     private void WhileFollowingPlayer()
     {
-        //Update the target pos to maintain a constant distance from the player
-        Vector3 diff = targetPos - target.transform.position;
-        diff.y = 0;
-        diff.Normalize();
-        diff *= followingDistance;
-        diff.y = followingHeight;
+        //Orbit
+        OrbitWithStick();
 
-        targetPos = target.transform.position + diff;
+        //Update the target pos to maintain a constant distance from the player
+        MaintainDistance(followingHeight, followingDistance);
 
         //Move towards the target pos
         MoveToTargetPos(followingMoveSpeed);
